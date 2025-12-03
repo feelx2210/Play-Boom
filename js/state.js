b94eccdb193ea113b5595a1cdb52e258980a61c7
feelx2210 <felixhuening@googlemail.com>
2025-12-03 21:05:51 +0100

    Create state.js

---
+++ js/state.js
@@ -0,0 +1,19 @@
+import { LEVELS } from './constants.js';
+
+export const state = {
+    grid: [],
+    items: [],
+    bombs: [],
+    particles: [],
+    players: [],
+    currentLevel: LEVELS.hell,
+    selectedCharIndex: 0,
+    selectedLevelKey: 'hell',
+    menuState: 0,
+    isGameOver: false,
+    isPaused: false,
+    hellFireTimer: 0,
+    hellFirePhase: 'IDLE', 
+    hellFireActive: false,
+    keys: {}
+};
